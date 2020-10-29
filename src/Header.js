81bd3c9c93b1cc45f93223a46c903d3e6a0fb86b
Johannes Kettmann <jkettmann@gmx.net>
2020-10-29 20:40:45 +0100

    Impplement header

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
+import { Container, Logo, HeaderLink } from './Header.style';
 
 function Header() {
   return (
-    <header>
-      Header Placeholder
-    </header>
+    <Container>
+      <Link to="/">
+        <Logo />
+      </Link>
+
+      <nav>
+        <HeaderLink to="/search/javascript">
+          Search
+        </HeaderLink>
+        <HeaderLink to="/#how-it-works" as={HashLink}>
+          How it works
+        </HeaderLink>
+        <HeaderLink to="/#about" as={HashLink}>
+          About
+        </HeaderLink>
+      </nav>
+    </Container>
   );
 }
 
